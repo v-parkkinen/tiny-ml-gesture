058eef69a1675bff75380eb8e1e396061c2c218a
Ville Parkkinen <ville.parkkinen@hotmail.com>
2024-03-10 21:22:43 +0200

    Add draft of acutal inference code

--- inference/inference.ino
+++ inference/inference.ino
@@ -1,51 +1,84 @@
+#include <Wire.h>
+
 #include "lstmCell.h"
 #include "denseLayer.h"
 #include "weights.h"
 
-float input[60] = {
-    0.4764710566127876, 0.31938762043024943, 0.6389980931754514, 0.5968458834687843, 0.5034561684596017, 0.4994125276569772,
-    0.4768691774822836, 0.3164840966081563, 0.6377739588973375, 0.5974595971806363, 0.5040817883573663, 0.49910734721904326,
-    0.4767099291344852, 0.3178038801636532, 0.636079003743026, 0.596604117461085, 0.5031967650873579, 0.4997177080949111,
-    0.4761525599171908, 0.317012010030355, 0.6373973021963795, 0.5964181436090086, 0.5033951323720149, 0.49915312428473335,
-    0.47599331156939245, 0.3181998152303022, 0.6372089738459004, 0.5970504547060683, 0.5040207522697795, 0.49948882276646067,
-    0.47623218409109, 0.31806783687475254, 0.6373973021963795, 0.5968272860835766, 0.5036392767223621, 0.499320973525597,
-    0.4760729357432916, 0.3174079450970041, 0.6371148096706608, 0.5965855200758773, 0.5034103913939116, 0.49950408178835737,
-    0.476630304960586, 0.316616074963706, 0.6369264813201818, 0.597013259935653, 0.5035324635690852, 0.49916838330663005,
-    0.47941715104705784, 0.3200475122079979, 0.6372089738459004, 0.5969574677800301, 0.5032883192187381, 0.49489585717555507,
-    0.4306075324468508, 0.457041045268576, 0.6321241083829657, 0.5987800115303789, 0.4981765468833448, 0.44972915236133365
+const int numClasses = 5;
+const char* classes[numClasses] = {
+  "default",
+  "wave",
+  "circle_cw",
+  "circle_ccw",
+  "rotate"
 };
+
+int inputSize =  6;
+int hiddenSize =  10;
+
+float output[numClasses];
+LSTMCell lstmCell(inputSize, hiddenSize);
+DenseLayer denseLayer(inputSize, numClasses);
+
+typedef struct {
+    int16_t AcX;
+    int16_t AcY;
+    int16_t AcZ;
+    int16_t GyX;
+    int16_t GyY;
+    int16_t GyZ;
+} GyroValues;
+
+const int MPU_addr=0x68;
+GyroValues latestValues;
+float preprocessedValues[6];
+
+void setupGyroscope() {
+  Wire.begin();
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+}
+
+GyroValues readGyro(GyroValues *values) {
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr,14,true); 
+
+  values->AcX = Wire.read()<<8|Wire.read();  
+  values->AcY = Wire.read()<<8|Wire.read();
+  values->AcZ = Wire.read()<<8|Wire.read();
+  Wire.read()<<8|Wire.read(); // Discard the temperature value
+  values->GyX = Wire.read()<<8|Wire.read();
+  values->GyY = Wire.read()<<8|Wire.read();
+  values->GyZ = Wire.read()<<8|Wire.read();
+}
+
+void preprocessGyroData(const GyroValues &values) {
+  // TODO: replace with actual preprocessing
+  preprocessedValues[0] = 0.1 * values.AcX;
+  preprocessedValues[1] = 0.1 * values.AcY;
+  preprocessedValues[2] = 0.1 * values.AcZ;
+  preprocessedValues[3] = 0.1 * values.GyX;
+  preprocessedValues[4] = 0.1 * values.GyY;
+  preprocessedValues[5] = 0.1 * values.GyZ;
+}
+
+int runInference(const float input[]) {
+    lstmCell.forward(input);
+    denseLayer.forward(lstmCell.hiddenState, output);
+}
 
 void setup() {
   Serial.begin(9600);
+  setupGyroscope();
 }
 
 void loop() {
-    int inputSize =  6;
-    int hiddenSize =  10;
-    int numClasses = 5;
-    int seqLen = 10;
-
-    // Create an LSTMCell object
-    LSTMCell lstmCell(inputSize, hiddenSize);
-    DenseLayer denseLayer(inputSize, numClasses);
-    float output[numClasses];
-
-    unsigned long startTime = millis();
-    for (int t = 0; t < seqLen; t++) {
-        lstmCell.forward(input + t*inputSize);
-    }
-    
-    denseLayer.forward(lstmCell.hiddenState, output);
-    unsigned long endTime = millis();
-
-    String resultString = "[";
-    for (int i = 0; i < numClasses; i++) {
-      resultString += String(output[i]);
-      resultString += " ";
-    }
-    resultString += "] "; 
-    unsigned long inferenceTime = endTime - startTime;
-    resultString += String(inferenceTime) + "ms";
-
-    Serial.println(resultString);
+  readGyro(&latestValues);
+  preprocessGyroData(latestValues);
+  int classIndex = runInference(preprocessedValues);
+  Serial.println(classes[classIndex]);
 }
